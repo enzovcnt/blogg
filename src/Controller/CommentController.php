4df383c365a3c98a7ee3472e2cf7819b46efe17d
Enzo <enzovcnt@gmail.com>
2025-04-24 15:45:46 +0200

    we can delete and edit comment

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -35,4 +35,49 @@ final class CommentController extends AbstractController
             'id' => $post->getId()
         ]);
     }
+
+    #[Route('/comment/delete/{id}', name: 'app_comment_delete')]
+    public function delete(Comment $comment, EntityManagerInterface $manager): Response
+    {
+        if(!$this->getUser() || !$comment)
+        {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $post = $comment->getPost();
+        if($comment->getAuthor() == $this->getUser())
+        {
+            $manager->remove($comment);
+            $manager->flush();
+        }
+        return $this->redirectToRoute('app_post_show', [
+            'id' => $post->getId()
+        ]);
+    }
+
+    #[Route('/comment/{id}/edit', name: 'app_comment_edit')]
+    public function edit(Request $request, Comment $comment, EntityManagerInterface $manager): Response
+    {
+        if(!$this->getUser() || !$comment)
+        {
+            return $this->redirectToRoute('app_login');
+        }
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+        $post = $comment->getPost();
+        if($form->isSubmitted() && $form->isValid()){
+            $comment->setAuthor($this->getUser());
+            $manager->persist($comment);
+            $manager->flush();
+            return $this->redirectToRoute('app_post_show', [
+                'id' => $post->getId()
+            ]);
+        }
+        return $this->render('comment/index.html.twig',[
+            'comment' => $comment,
+            'formCommentEdit' => $form->createView(),
+        ]);
+
+    }
+
 }
